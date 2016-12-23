defb186313de02fa42f8672d8a238ddea00421c1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-12-23 15:12:01 +0900

    とりあえずウィンドウを表示

---
+++ Assets/Editor/ReorderbleComponents.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Collections.Generic;
+
+namespace HK.Framework.Editor
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class ReorderbleComponents : EditorWindow
+	{
+		private ReorderableList reorderbleList;
+
+		private GameObject target;
+
+		[MenuItem("Window/Reorderble Components")]
+		private static void OnOpen()
+		{
+			var window = EditorWindow.GetWindow(typeof(ReorderbleComponents), false, "Reorderble Components") as ReorderbleComponents;
+			window.Show();
+		}
+
+		void OnGUI()
+		{
+			if(this.target == null)
+			{
+				GUILayout.Label("Please Select GameObject");
+			}
+			else
+			{
+				
+			}
+		}
+
+		void OnSelectionChange()
+		{
+			this.target = Selection.activeGameObject;
+			this.Repaint();
+		}
+	}
+}
