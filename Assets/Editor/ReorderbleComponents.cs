61996ad37310c53c8b0c6de43f95b2183d7859c2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-12-24 17:55:33 +0900

    コンポーネントを列挙するだけ

--- Assets/Editor/ReorderbleComponents.cs
+++ Assets/Editor/ReorderbleComponents.cs
@@ -3,6 +3,7 @@ using UnityEngine.Assertions;
 using UnityEditor;
 using UnityEditorInternal;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HK.Framework.Editor
 {
@@ -14,6 +15,8 @@ namespace HK.Framework.Editor
 		private ReorderableList reorderbleList;
 
 		private GameObject target;
+
+		private List<Component> components;
 
 		[MenuItem("Window/Reorderble Components")]
 		private static void OnOpen()
@@ -30,7 +33,7 @@ namespace HK.Framework.Editor
 			}
 			else
 			{
-				
+				this.reorderbleList.DoLayoutList();
 			}
 		}
 
@@ -38,6 +41,24 @@ namespace HK.Framework.Editor
 		{
 			this.target = Selection.activeGameObject;
 			this.Repaint();
+			if(this.target == null)
+			{
+				return;
+			}
+
+			this.components = this.target.GetComponents(typeof(Component)).Where(c => c.GetType() != typeof(Transform)).ToList();
+			this.reorderbleList = new ReorderableList(this.components, typeof(Component), true, true, false, false);
+			this.reorderbleList.drawHeaderCallback = (rect) =>
+			{
+				EditorGUI.LabelField(rect, this.target.name);
+			};
+			this.reorderbleList.drawElementCallback = (rect, index, isActive, isFocused) =>
+			{
+				var component = this.components[index];
+				var content = new GUIContent(EditorGUIUtility.ObjectContent(component, component.GetType()));
+				content.text = component.GetType().Name;
+				EditorGUI.LabelField(rect, content);
+			};
 		}
 	}
 }
